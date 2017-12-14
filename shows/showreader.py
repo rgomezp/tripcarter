2d4429539a83c1e9b2552548c407bfbbd47044c8
Rodrigo Gomez Palacio <rogomezpalacio@gmail.com>
2017-12-14 01:27:48 -0600

    updated showreader to handle dashes  and slashes

--- shows/showreader.py
+++ shows/showreader.py
@@ -1,13 +1,23 @@
 import csv
 
-output = open('output.txt','w')
+output = open('output.html','w')
 
 with open('shows.csv', 'rb') as csvfile:
   showreader = csv.reader(csvfile.read().splitlines(), delimiter=',')
   for row in showreader:
+      date = ""
+      dash = False
+      for char in row[0]:
+          if char == '-' or char == '/':
+              dash = True
+          if dash:
+              date = date+"."
+          else:
+              date = date+char
+          dash = False
       #date
-      output.write("<tr>\n\t<td style=\"float:left;\">"+row[0]+"</td>\n")
+      output.write("<tr>\n\t<td style=\"float:left;\">"+date+"</td>\n")
       #city
       output.write("\t<td>"+row[1]+"</td>\n")
       #url
-      output.write("\t<td style = \"float: right; margin-right: 10px;\"><a href=\""+row[2]+" style = \"display: inline; border-style: solid; border-width: 2px; width: 100%; margin-left: 15px; color: white; padding:6px; padding-left: 20px; padding-right: 20px;\">Tickets</a></td>\n</tr>")
+      output.write("\t<td style = \"float: right; margin-right: 10px;\"><a href=\""+row[2]+"\" style = \"display: inline; border-style: solid; border-width: 2px; width: 100%; margin-left: 15px; color: white; padding:6px; padding-left: 20px; padding-right: 20px;\">Tickets</a></td>\n</tr>")
